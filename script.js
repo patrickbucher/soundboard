78775e8fd8b1e358a4e2a2ed3a81f2f089103e84
Patrick Bucher <patrick.bucher@mailbox.org>
2023-05-17 16:34:15 +0200

    button magic

--- script.js
+++ script.js
@@ -6,13 +6,23 @@ document.addEventListener("DOMContentLoaded", () => {
         if (sd.hasAttribute("data-shortcut")) {
             const shortcut = sd.getAttribute("data-shortcut");
             const title = sd.querySelector("h2");
-            title.innerHTML = `[${shortcut}] ${title.innerHTML}`;
+            title.innerHTML = `<tt>[${shortcut}]</tt> ${title.innerHTML}`;
             document.addEventListener("keydown", (e) => {
                 if (e.key == shortcut) {
-                    const audio = sd.querySelector("audio");
-                    audio.play();
+                    button.classList.add("active");
+                    setTimeout(() => {
+                        button.classList.remove("active");
+                    }, 200);
+                    button.click();
                 }
             });
         };
+        const button = sd.querySelector("button");
+        if (button) {
+            button.addEventListener("click", (e) => {
+                const audio = sd.querySelector("audio");
+                audio.play();
+            });
+        }
     });
 });
